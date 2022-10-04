524aebc513f25ec0f46926a5b2362669d617eda5
Leonardo <leonardorsm07@gmail.com>
2022-10-04 16:50:20 -0300

    modificação geral

--- usuarioArquivo.php
+++ usuarioArquivo.php
@@ -2,11 +2,13 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Arquivo Usuario AV1</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <form action="usuarioArquivo.php" method="POST" enctype="multipart/form-data" >
+  <p>insira o arquivo de texto com os usuários</p>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+    
 </head>
 <body>
     
